5e7ba3d0498eb04b9f7c600102e9f017ca4fc4e5
MichalDolchon <michal.dolchon@veriori.com>
2023-01-31 14:24:07 +0100

    Added comment.

--- services/lottery/utils.ts
+++ services/lottery/utils.ts
@@ -1,6 +1,16 @@
-function hasField<T>(fields: string[], field: T): boolean {
-	return fields.some((req: string) => Object.prototype.hasOwnProperty.call(field, req)
+/**
+ * Check if object property exist in array.
+ *
+ * @function
+ * @template T, V
+ * @param {T} object - any object
+ * @param {T[]<V extends T & PropertyKey>} properties - property to look for
+ *
+ * @returns boolean
+ * */
+function hasProperty<T, V extends T & PropertyKey>(object: T, properties: V[]): boolean {
+	return properties.some((prop: V) => Object.prototype.hasOwnProperty.call(object, prop)
 	)
 }
 
-export { hasField }
+export { hasProperty }

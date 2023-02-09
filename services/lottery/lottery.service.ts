f2142f43c49d93d25a93021723eed40711f943c6
Szymon Reddig <mixowo@hotmail.com>
2023-02-09 17:28:04 +0100

    Added wallet_post field

--- services/lottery/lottery.service.ts
+++ services/lottery/lottery.service.ts
@@ -4,14 +4,14 @@ import MongooseAdapter from "moleculer-db-adapter-mongoose";
 import { ERC20_TYPE, TOKEN_DISTRIBUTION_METHOD, TOKEN_TYPE } from "./enums";
 import type { ITwitter } from "./interfaces/twitter";
 import { lottery } from "./lottery";
-import { hasProperty } from "./utils"
+import { hasProperty } from "./utils";
 
 const regex = {
 	twitter: {
 		post: "^(http(s)?:\\/\\/)?twitter\\.com\\/(?:#!\\/)?(\\w+)\\/status(es)?\\/(\\d+)$",
 		username: "^@(\\w{1,15})$",
-	}
-}
+	},
+};
 
 const LotteryService: ServiceSchema = {
 	name: "lottery",
@@ -21,120 +21,165 @@ const LotteryService: ServiceSchema = {
 	adapter: new MongooseAdapter(`mongodb+srv://${process.env.MONGO_URI}`, {
 		user: process.env.MONGO_USER,
 		pass: process.env.MONGO_PASS,
-		keepAlive: true
+		keepAlive: true,
 	}),
 	model: lottery,
 
 	settings: {
-		fields: ["duration", "distribution_method", "number_of_tokens", "distribution_options", "fees_amount", "num_of_winners", "asset_choice", "twitter", "lottery_end"],
+		fields: [
+			"duration",
+			"distribution_method",
+			"number_of_tokens",
+			"distribution_options",
+			"fees_amount",
+			"num_of_winners",
+			"asset_choice",
+			"twitter",
+			"lottery_end",
+		],
 
 		entityValidator: {
-			duration:
-				{ type: "number", integer: true, positive: true },
-			distribution_method:
-				{ 
-					type: "enum",
-					values: Object.values(TOKEN_DISTRIBUTION_METHOD),
-					optional: true,
-					custom: (value: TOKEN_DISTRIBUTION_METHOD, errors: any[], schema: any, name: any, parent: any, context: any): (TOKEN_DISTRIBUTION_METHOD | undefined) => {
-						if (context.data.asset_choice !== TOKEN_TYPE.ERC721) {
-							if (value) {
-								return value;
-							}
-							errors.push({type: "required"});
+			duration: { type: "number", integer: true, positive: true },
+			distribution_method: {
+				type: "enum",
+				values: Object.values(TOKEN_DISTRIBUTION_METHOD),
+				optional: true,
+				custom: (
+					value: TOKEN_DISTRIBUTION_METHOD,
+					errors: any[],
+					schema: any,
+					name: any,
+					parent: any,
+					context: any,
+				): TOKEN_DISTRIBUTION_METHOD | undefined => {
+					if (context.data.asset_choice !== TOKEN_TYPE.ERC721) {
+						if (value) {
+							return value;
 						}
-						return undefined;
+						errors.push({ type: "required" });
 					}
+					return undefined;
 				},
-			distribution_options:
-				{
-					type: "array",
-					items: {
-						type: "number",
-						positive: true
-					},
-					optional: true,
-					custom: (value: number[], errors: any[], schema: any, name: any, parent: any, context: any): (number[] | undefined) => {
-						if (context.data.asset_choice !== TOKEN_TYPE.ERC721 && context.data.distribution_method === TOKEN_DISTRIBUTION_METHOD.PERCENTAGE) {
-							if (value) {
-								if (value.reduce((sum, val) => sum + val, 0) === 100) {
-									return value
-								}
-								errors.push({type: "numberEqual", expected: 100 });
-							} else {
-								errors.push({type: "required"});
-							}
-						}
-						return undefined;
-					}
-				},
-			fees_amount: 
-				{ type: "number" },
-			number_of_tokens:
-				{
+			},
+			distribution_options: {
+				type: "array",
+				items: {
 					type: "number",
 					positive: true,
-					optional: true,
-					custom: (value: number, errors: any[], schema: any, name: any, parent: any, context: any): (number | undefined) => {
-						if (context.data.asset_choice !== TOKEN_TYPE.ERC721) {
-							if (value) {
+				},
+				optional: true,
+				custom: (
+					value: number[],
+					errors: any[],
+					schema: any,
+					name: any,
+					parent: any,
+					context: any,
+				): number[] | undefined => {
+					if (
+						context.data.asset_choice !== TOKEN_TYPE.ERC721 &&
+						context.data.distribution_method === TOKEN_DISTRIBUTION_METHOD.PERCENTAGE
+					) {
+						if (value) {
+							if (value.reduce((sum, val) => sum + val, 0) === 100) {
 								return value;
 							}
-							errors.push({type: "required"});
+							errors.push({ type: "numberEqual", expected: 100 });
+						} else {
+							errors.push({ type: "required" });
 						}
-						return undefined;
 					}
+					return undefined;
 				},
-			wallet:
-				{ type: "startsWith", expected: "0x", length: 42 },
-			num_of_winners:
-				{ type: "number", integer: true, positive: true },
-			asset_choice:
-				{ type: "enum", values: Object.values(TOKEN_TYPE) },
-			erc20_choice:
-				{
-					type: "enum",
-					values: Object.values(ERC20_TYPE),
-					optional: true,
-					custom: (value: string, errors: any[], schema: any, name: any, parent: any, context: any): (string | undefined)  => {
-						if (context.data.asset_choice === TOKEN_TYPE.ERC20) {
-							if (value) {
-								return value;
-							}
-							errors.push({type: "required"});
+			},
+			fees_amount: { type: "number" },
+			number_of_tokens: {
+				type: "number",
+				positive: true,
+				optional: true,
+				custom: (
+					value: number,
+					errors: any[],
+					schema: any,
+					name: any,
+					parent: any,
+					context: any,
+				): number | undefined => {
+					if (context.data.asset_choice !== TOKEN_TYPE.ERC721) {
+						if (value) {
+							return value;
 						}
-						return undefined;
+						errors.push({ type: "required" });
 					}
+					return undefined;
 				},
-			nfts_choice:
-				{
-					type: "array",
-					items: {
-						type: "object",
-						props: {
-							name: { type: "string" },
-							token_id: { type: "number", integer: true, positive: true },
-							contract_address: { type: "string", required: true }
+			},
+			wallet: { type: "startsWith", expected: "0x", length: 42 },
+			num_of_winners: { type: "number", integer: true, positive: true },
+			asset_choice: { type: "enum", values: Object.values(TOKEN_TYPE) },
+			erc20_choice: {
+				type: "enum",
+				values: Object.values(ERC20_TYPE),
+				optional: true,
+				custom: (
+					value: string,
+					errors: any[],
+					schema: any,
+					name: any,
+					parent: any,
+					context: any,
+				): string | undefined => {
+					if (context.data.asset_choice === TOKEN_TYPE.ERC20) {
+						if (value) {
+							return value;
 						}
+						errors.push({ type: "required" });
+					}
+					return undefined;
+				},
+			},
+			nfts_choice: {
+				type: "array",
+				items: {
+					type: "object",
+					props: {
+						name: { type: "string" },
+						token_id: { type: "number", integer: true, positive: true },
+						contract_address: { type: "string", required: true },
 					},
-					optional: true,
-					custom: (value: object[], errors: any[], schema: any, name: any, parent: any, context: any): (object[] | undefined) => {
-						if (context.data.asset_choice === TOKEN_TYPE.ERC721) {
-							if (value) {
-								return value;
-							}
-							errors.push({type: "required"});
-						}
-						return undefined;
-					}
 				},
+				optional: true,
+				custom: (
+					value: object[],
+					errors: any[],
+					schema: any,
+					name: any,
+					parent: any,
+					context: any,
+				): object[] | undefined => {
+					if (context.data.asset_choice === TOKEN_TYPE.ERC721) {
+						if (value) {
+							return value;
+						}
+						errors.push({ type: "required" });
+					}
+					return undefined;
+				},
+			},
 			twitter: {
 				type: "object",
 				optional: false,
-				custom: (value: object, errors: any[], schema: any, name: any, parent: any, context: any): object => {
+				custom: (
+					value: object,
+					errors: any[],
+					schema: any,
+					name: any,
+					parent: any,
+					context: any,
+				): object => {
 					const twitterReq: (keyof ITwitter)[] = ["content", "follow", "like", "retweet"];
 					if (!hasProperty(context.data.twitter, twitterReq)) {
-						errors.push({type: "twitterFieldRequired"});
+						errors.push({ type: "twitterFieldRequired" });
 					}
 					return value;
 				},
@@ -142,24 +187,23 @@ const LotteryService: ServiceSchema = {
 					like: {
 						type: "string",
 						pattern: regex.twitter.post,
-						optional: true
+						optional: true,
 					},
 					content: { type: "string", min: 3, max: 280, optional: true },
 					retweet: {
 						type: "string",
 						pattern: regex.twitter.post,
-						optional: true
+						optional: true,
 					},
-					follow: { type: "string", pattern: regex.twitter.username, optional: true }
-				}
+					follow: { type: "string", pattern: regex.twitter.username, optional: true },
+					wallet_post: { type: "string", pattern: regex.twitter.post, optional: false },
+				},
 			},
-			$$strict: "remove"
-		}
+			$$strict: "remove",
+		},
 	},
 
-	dependencies: [
-		{ name: "twitter", version: 1 }
-	],
+	dependencies: [{ name: "twitter", version: 1 }],
 
 	actions: {},
 
@@ -174,7 +218,7 @@ const LotteryService: ServiceSchema = {
 	methods: {
 		async checkEndedLotteries() {
 			// TODO
-		}
+		},
 	},
 
 	/**
@@ -186,14 +230,14 @@ const LotteryService: ServiceSchema = {
 	 * Service started lifecycle event handler
 	 */
 	started() {
-		this.checkEndedLotteries()
+		this.checkEndedLotteries();
 		// todo iterate through lotteries in db and retrieve end time.
 	},
 
 	/**
 	 * Service stopped lifecycle event handler
 	 */
-	async stopped() {}
-}
+	async stopped() {},
+};
 
 export default LotteryService;
--- services/lottery/lottery.ts
+++ services/lottery/lottery.ts
@@ -4,123 +4,136 @@ import type { IERC20, IERC721, IMATIC } from "./interfaces/lottery";
 import type { ITwitter } from "./interfaces/twitter";
 import { hasProperty } from "./utils";
 
+export type LotteryDTO = (IERC20 | IERC721 | IMATIC) & { twitter: ITwitter; fees_amount: number };
+export type LotteryEntity = LotteryDTO & { lottery_end: Date; fees_amount: number };
 
-export type LotteryDTO = (IERC20 | IERC721 | IMATIC) & { twitter: ITwitter, fees_amount: number };
-export type LotteryEntity = LotteryDTO & { lottery_end: Date, fees_amount: number };
+type LotterySettings = IERC20 &
+	IERC721 &
+	IMATIC & { twitter: ITwitter; lottery_end: Date; fees_amount: number; active: boolean };
 
-type LotterySettings = IERC20 & IERC721 & IMATIC & { twitter: ITwitter, lottery_end: Date, fees_amount: number, active: boolean };
-
-const lotterySchema = new Schema<LotterySettings>({
-	_id: {
-		type: Number
-	},
-	duration: {
-		type: Number,
-		required: true
-	},
-	lottery_end: {
-		type: Date,
-		default: undefined
-	},
-	distribution_method: {
-		type: String,
-		enum: TOKEN_DISTRIBUTION_METHOD,
-		required: false,
-		default: undefined
-	},
-	distribution_options: {
-		type: [Number],
-		required: false,
-		default: undefined
-	},
-	number_of_tokens: {
-		type: Number,
-		required: false,
-		default: undefined
-	},
-	fees_amount: {
-		type: Number,
-	},
-	wallet: {
-		type: String,
-		required: true
-	},
-	num_of_winners: {
-		type: Number,
-		required: true
-	},
-	asset_choice: {
-		type: String,
-		enum: TOKEN_TYPE,
-		required: true
-	},
-	erc20_choice: {
-		type: String,
-		enum: ERC20_TYPE,
-		default: undefined,
-		required: [
-			function(this: IERC20) { return this.asset_choice === TOKEN_TYPE.ERC20 },
-			'erc20_choice is required if asset_choice is ERC20'
-		],
-	},
-	nfts_choice: {
-		type: [{ name: String, token_id: Number, contract_address: String }],
-		default: undefined,
-		required: [
-			function(this: IERC721) { return this.asset_choice === TOKEN_TYPE.ERC721 },
-			'nfts_choice is required if asset_choice is ERC721'
-		]
-	},
-	twitter: {
-		type: Object,
-		required: true,
-		like: {
-			type: String,
-			required: false,
-			default: undefined
+const lotterySchema = new Schema<LotterySettings>(
+	{
+		_id: {
+			type: Number,
 		},
-		content: {
-			type: String,
-			required: false,
-			default: undefined
+		duration: {
+			type: Number,
+			required: true,
 		},
-		retweet: {
-			type: String,
-			required: false,
-			default: undefined
+		lottery_end: {
+			type: Date,
+			default: undefined,
 		},
-		follow: {
+		distribution_method: {
 			type: String,
+			enum: TOKEN_DISTRIBUTION_METHOD,
 			required: false,
-			default: undefined
-		}
+			default: undefined,
+		},
+		distribution_options: {
+			type: [Number],
+			required: false,
+			default: undefined,
+		},
+		number_of_tokens: {
+			type: Number,
+			required: false,
+			default: undefined,
+		},
+		fees_amount: {
+			type: Number,
+		},
+		wallet: {
+			type: String,
+			required: true,
+		},
+		num_of_winners: {
+			type: Number,
+			required: true,
+		},
+		asset_choice: {
+			type: String,
+			enum: TOKEN_TYPE,
+			required: true,
+		},
+		erc20_choice: {
+			type: String,
+			enum: ERC20_TYPE,
+			default: undefined,
+			required: [
+				function (this: IERC20) {
+					return this.asset_choice === TOKEN_TYPE.ERC20;
+				},
+				"erc20_choice is required if asset_choice is ERC20",
+			],
+		},
+		nfts_choice: {
+			type: [{ name: String, token_id: Number, contract_address: String }],
+			default: undefined,
+			required: [
+				function (this: IERC721) {
+					return this.asset_choice === TOKEN_TYPE.ERC721;
+				},
+				"nfts_choice is required if asset_choice is ERC721",
+			],
+		},
+		twitter: {
+			type: Object,
+			required: true,
+			like: {
+				type: String,
+				required: false,
+				default: undefined,
+			},
+			content: {
+				type: String,
+				required: false,
+				default: undefined,
+			},
+			retweet: {
+				type: String,
+				required: false,
+				default: undefined,
+			},
+			follow: {
+				type: String,
+				required: false,
+				default: undefined,
+			},
+			wallet_post: {
+				type: String,
+				required: true,
+				default: undefined,
+			},
+		},
+		active: {
+			type: Boolean,
+			required: false,
+			default: true,
+		},
 	},
-	active: {
-		type: Boolean,
-		required: false,
-		default: undefined
-	}
-}, {
-	timestamps: true
-});
+	{
+		timestamps: true,
+	},
+);
 
-lotterySchema.pre('validate', function(next) {
+lotterySchema.pre("validate", function (next) {
 	const twitterReq: (keyof ITwitter)[] = ["content", "follow", "like", "retweet"];
 	if (hasProperty(this.toObject().twitter, twitterReq)) {
-		return next()
+		return next();
 	}
-	return next(new Error('At least one Twitter requirement should be defined.'));
+	return next(new Error("At least one Twitter requirement should be defined."));
 });
 
 /**
  * todo set lottery id
  * */
-lotterySchema.pre('save', function(next) {
+lotterySchema.pre("save", function (next) {
 	// set lottery id
 	this._id = 2;
 
 	// set lottery end date
-	const milliseconds = new Date().getTime() + (this.duration * 60 * 60 * 1000);
+	const milliseconds = new Date().getTime() + this.duration * 60 * 60 * 1000;
 	this.lottery_end = new Date(milliseconds);
 
 	next();
